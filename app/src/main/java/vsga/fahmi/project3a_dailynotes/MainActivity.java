3e77b91a766a06567ea2155006543e9906199ece
Fahmi Fauzy Ridwan <fahmifauzyffr20@gmail.com>
2021-08-15 21:00:40 +0700

    Innitial Commit

---
+++ app/src/main/java/vsga/fahmi/project3a_dailynotes/MainActivity.java
@@ -0,0 +1,46 @@
+package vsga.fahmi.project3a_dailynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    public static final int REQUEST_CODE_STORAGE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Daily Notes");
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, InsertAndViewActivity.class);
+                Map<String, Object> data = (Map<String, Object>) parent.getAdapter().getItem(position);
+                intent.putExtra("filename", data.get("name").toString());
+                Toast.makeText(MainActivity.this, "You clicked" + data.get("name"), Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long l) {
+                Map<String, Object> data = (Map<String, Object>) parent.getAdapter().getItem(position);
+                tampilkanDialogKonfirmasiHapusCatatan(data.get("name").toString());
+                return true;
+            }
+        });
+    }
+}
